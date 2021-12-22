36c2d92ea2e4e4ec9e2d166721d659c89baf2f8d
lackynasta <lackynasta@gmail.com>
2021-12-22 10:46:02 +0300

    implementation fonctionnalité READ

---
+++ tests/test_views.py
@@ -0,0 +1,22 @@
+# tests/test_views.py
+from flask_testing import TestCase
+from wsgi import app
+
+class TestViews(TestCase):
+    def create_app(self):
+        app.config['TESTING'] = True
+        return app
+
+    def test_read_many_products(self):
+        response = self.client.get("/api/v1/produits")
+        products = response.json
+        self.assertIsInstance(products, list)
+        self.assertGreater(len(products), 2) # 2 is not a mistake here.
+
+    def test_read_product(self):
+        response = self.client.get("/api/v1/produit/1")
+        product = response.json
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(product, dict)
+        self.assertEqual(product['name'], 'Skello')
+
