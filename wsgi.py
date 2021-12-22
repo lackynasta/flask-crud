cc77ef90835e4f2d98aea12bc5a6173ff533c523
lackynasta <lackynasta@gmail.com>
2021-12-22 11:46:53 +0300

    optimisation de code suite au retour de pylint

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,5 @@
 # wsgi.py
 # pylint: disable=missing-docstring
-
 from flask import Flask
 from flask import jsonify
 from flask import abort
@@ -13,16 +12,20 @@ def hello():
 
 @app.route('/api/v1/produits')
 def read_many_products():
-    PRODUCTS = [{ 'id': 1, 'name': 'Skello' },{ 'id': 2, 'name': 'Socialive.tv' },{'id': 3, 'name': 'Learn'}]
-    return jsonify(PRODUCTS)
+    products = [{ 'id': 1, 'name': 'Skello' },
+    { 'id': 2, 'name': 'Socialive.tv' },
+    {'id': 3, 'name': 'Learn'}]
+    return jsonify(products)
 
 @app.route('/api/v1/produit/<int:product_id>')
 def read_product(product_id):
-    PRODUCTS = [{ 'id': 1, 'name': 'Skello' },{ 'id': 2, 'name': 'Socialive.tv' },{'id': 3, 'name': 'Learn'}]
+    products = [{ 'id': 1, 'name': 'Skello' },
+    { 'id': 2, 'name': 'Socialive.tv' },
+    {'id': 3, 'name': 'Learn'}]
     produit = {}
-    for product in PRODUCTS:
+    for product in products:
         if product['id'] == product_id:
             produit = product
-    if produit == {}:
+    if not produit:
         abort(404)
     return produit
