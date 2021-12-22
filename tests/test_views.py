837f6ed5468322ad825ce1093c8b7317be7f8244
lackynasta <lackynasta@gmail.com>
2021-12-22 14:06:22 +0300

    implementation fonctionnalité UPDATE

--- tests/test_views.py
+++ tests/test_views.py
@@ -43,3 +43,70 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 201)
         self.assertIsInstance(product, dict)
         self.assertEqual(product['name'], 'Youtube')
+
+    def test_create_product_validation_error(self):
+        response_1 = self.client.post("/api/v1/produit", json={'name': 2})
+        product_1 = response_1.json
+        self.assertEqual(response_1.status_code, 422)
+        self.assertIsNone(product_1)
+
+        response_2 = self.client.post("/api/v1/produit", json={'name': ''})
+        product_2 = response_2.json
+        self.assertEqual(response_2.status_code, 422)
+        self.assertIsNone(product_2)
+
+    def test_create_product_bad_request(self):
+        response_1 = self.client.post("/api/v1/produit", json={'other': 2})
+        product_1 = response_1.json
+        self.assertEqual(response_1.status_code, 400)
+        self.assertIsNone(product_1)
+
+        response_2 = self.client.post("/api/v1/produit", json={'other': 'what'})
+        product_2 = response_2.json
+        self.assertEqual(response_2.status_code, 400)
+        self.assertIsNone(product_2)
+
+        response_3 = self.client.post("/api/v1/produit")
+        product_3 = response_3.json
+        self.assertEqual(response_3.status_code, 400)
+        self.assertIsNone(product_3)
+
+    def test_update_product(self):
+        update_response = self.client.patch("/api/v1/produit/1", json={'name': 'Netlify'})
+        update_product = update_response.json
+        self.assertEqual(update_response.status_code, 204)
+        self.assertIsNone(update_product)
+
+        read_response = self.client.get("/api/v1/produit/1")
+        product = read_response.json
+        self.assertEqual(read_response.status_code, 200)
+        self.assertIsInstance(product, dict)
+        self.assertEqual(product['name'], 'Netlify')
+
+    def test_update_product_not_found(self):
+        response = self.client.patch("/api/v1/produit/20", json={'name': 'Doctolib'})
+        product = response.json
+        self.assertEqual(response.status_code, 404)
+        self.assertIsNone(product)
+
+    def test_update_product_validation_error(self):
+        response_1 = self.client.patch("/api/v1/produit/1", json={'name': 2})
+        product_1 = response_1.json
+        self.assertEqual(response_1.status_code, 422)
+        self.assertIsNone(product_1)
+
+        response_2 = self.client.patch("/api/v1/produit/1", json={'name': ''})
+        product_2 = response_2.json
+        self.assertEqual(response_2.status_code, 422)
+        self.assertIsNone(product_2)
+
+    def test_update_product_bad_request(self):
+        response_1 = self.client.patch("/api/v1/produit/1", json={'other': 'what'})
+        product_1 = response_1.json
+        self.assertEqual(response_1.status_code, 400)
+        self.assertIsNone(product_1)
+
+        response_2 = self.client.patch("/api/v1/produit/1")
+        product_2 = response_2.json
+        self.assertEqual(response_2.status_code, 400)
+        self.assertIsNone(product_2)
